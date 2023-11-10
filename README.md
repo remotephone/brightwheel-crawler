161e5b4bdff34e6e02f6f1ae36733afc6be0be8f
remotephone <remotephone@gmail.com>
2023-11-10 12:22:45 -0600

    v2

--- README.md
+++ README.md
@@ -1,26 +1,42 @@
-# brightwheel-crawler
-This python script uses selenium to crawl the page, loads all images, and then passes the auth info to requests and downloads all the image files. 
+# brightwheel-crawler v2
+
+This python script uses selenium via <https://github.com/ultrafunkamsterdam/undetected-chromedriver> to crawl the page, loads all images, and then passes the auth info to requests and downloads all the image files.
 
 ### History
-My kid will leave a school that uses the brightwheel app and I wanted to make sure I got all the pictures before he left. 
+
+My kid will leave a school that uses the brightwheel app and I wanted to make sure I got all the pictures before he left.
+
+I had an old version I put up never thinking anyone would use it, but I've been thrilled to hear how much use folks are getting out of it enough so that they've opened PRs and issues. Its pretty exciting to see something I threw together when I was really bad at writing code be used and appreciated by others. I've slightly less worse at python than I was when I first wrote this and the Brightwheel page has gotten noteably more reistant to scraping, so I've updated how this works and what it does.
 
 ### Future Plans
-I want to add image recognition to this to be able to parse out my kids face or remove ones that are likely just updates from the school, maybe that will come later.
+
+I _used to_ want to add image recognition to this to be able to parse out my kids face or remove ones that are likely just updates from the school, maybe that will come later. Nowadays, I'm happy to help this keep limping along as long as I have access to the site and can test changes.
 
 ### Requirements
-1. Firefox needs to be installed on the same device
-2. pyyaml must be installed (python -m pip install pyyaml)
-3. This was known to work with Python3.10 and FireFox 100.0.1.  No guarantees with other versions of either application.
+
+1. Python 3.11+ - other versions might work but I am not using them
+2. pipenv
+3. A brightwheel account with a kid registered
+
+### Installation
+
+1. Clone this repo
+2. `pipenv install`
+3. `pipenv shell`
 
 ### How to use
+
 1. Rename sample-config.yml to config.yml
 2. Update config.yml with the proper email and password
 3. Update config.yml with the dates your child/ren was/were enrolled and used Brightwheel
-4. Run brightscraper.py
-5. FireFox will open and login automatically.
-6. Click the child you wish to download photos.
-7. Wait until the script stops (returns to command prompt) and close FireFox
+4. Run `brightscraper.py -c` to use the undetected chrome driver or `-e` to connect to a debuggable instance of chrome (see <https://github.com/remotephone/brightwheel-crawler/issues/5#issuecomment-1711859342>)
+5. Chrome will open and login automatically.
+6. You may be prompted for a captcha challenge. If so, I don't think this will work yet and you need to close it out and try again
+7. You may be prompte for a 2FA code, enter it reasonably quickly (have your email account ready)
+8. If you did not pass an index number for the kid, you will be prompted to select one
+9. Wait until the script stops (returns to command prompt) and close Chrome
 
 ### Limitations
-1. With multiple children, you will need to run this multiple times and click on the child to download each time
-2. Photos are stored with the raw filename and do not store the date
+
+1. Photos are stored with the raw filename and do not store the date
+2. Probably some others, please open an issue
--- brightscraper.py
+++ brightscraper.py
@@ -2,8 +2,12 @@ import logging
 import re
 import time
 import yaml
+import argparse
+import random
 
 import requests
+import undetected_chromedriver as uc
+
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,22 +22,38 @@ for images, find all of them, pass the cookies to requests, and then download
 all images in bulk. Works with current site design as off 6/24/19"""
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+file_handler = logging.FileHandler("scraper.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
 def config_parser():
     """parse config file in config.yml if present"""
     try:
-        with open("config.yml", 'r') as config:
-            cfg = yaml.safe_load(config)
-        username = cfg['bwuser']
-        password = cfg['bwpass']
-        signin_url = cfg['bwsignin']
-        kidlist_url = cfg['bwlist']
-        startdate = cfg['startdate']
-        enddate = cfg['enddate']
+        with open("config.yml", "r") as config:
+            config = yaml.safe_load(config)
     except FileNotFoundError:
-        logging.error('[!] No config file found, check config file!')
+        logger.error("[!] No config file found, check config file!")
         raise SystemExit
 
-    return username, password, signin_url, kidlist_url, startdate, enddate
+    return config
+
+
+def get_random_time():
+    """Randomly wait before sending imput, some time under 4 seconds"""
+    random_wait = random.randint(0, 2000)
+    return random_wait / 1000
 
 
 # Get the first URL and populate the fields
@@ -41,158 +61,203 @@ def signme_in(browser, username, password, signin_url):
     """Populate and send login info using U/P from config"""
 
     browser.get(signin_url)
-    time.sleep(2)
+    time.sleep(get_random_time())
     loginuser = browser.find_element(By.XPATH, '//input[@id="username"]')
-    loginpass = browser.find_element(By.ID, 'password')
+    loginpass = browser.find_element(By.ID, "password")
     loginuser.click()
-    time.sleep(2)
+    time.sleep(get_random_time())
     loginuser.send_keys(username)
     loginpass.click()
-    time.sleep(2)
+    time.sleep(get_random_time())
     loginpass.send_keys(password)
 
     # Submit login, have to wait for page to change
     try:
         loginpass.submit()
-        WebDriverWait(browser, 5).until(EC.url_changes(signin_url))
+        WebDriverWait(browser, 45).until(EC.url_changes(signin_url))
     except:
-        logging.error('[!] - Unable to authenticate - Check credentials')
+        logger.error("[!] - Unable to authenticate - Check credentials")
         raise SystemExit
-
+    time.sleep(get_random_time() * 3)  # Change this to the amount of time you need to solve the captcha manually
     return browser
 
 
-def pic_finder(browser, kidlist_url, startdate, enddate):
-    """ This is the core logic of the script, navigate through the site, find
+def pic_finder(browser, kidlist_url, startdate, enddate, args):
+    """This is the core logic of the script, navigate through the site, find
     the page with photos, scroll to the bottom to load them all, load them all
     in a specified date range, and create an iterable list of image URLs"""
 
     browser.get(kidlist_url)
 
-    time.sleep(3)
+    time.sleep(get_random_time())
 
     # This xpath is generic enough to find any student listed.
     # You need to iterate through a list you create if you have more than one
     try:
-        students = browser.find_element(By.XPATH,
-            "//a[contains(@href, '/students/')]"
-            )
-        profile_url = students.get_property('href')
-        browser.get(profile_url)
-    except:
-        logging.error('[!] - Unable to find profile page, check target')
+        students = browser.find_elements(By.XPATH, "//a[contains(@href, '/students/')]")
+        lo("got students")
+        if not students:
+            raise Exception("No student URLs found")
+        if args.student_number:
+            profile_url = students[args.student_number - 1].get_property("href")
+        else:
+            try:
+                logger.info("Select a student:")
+                for i, student in enumerate(students):
+                    logger.info(f"{i}. {student.text}")
+                selection = int(input("Enter a number: "))
+                profile_url = students[selection].get_property("href")
+                # Replace last part of url path with feed
+            except Exception as e:
+                logger.error(f"[!] - Unable to find student: {e}")
+                raise SystemExit
+
+    except Exception as e:
+        logger.error(f"[!] - Unable to find profile page: {e}")
         raise SystemExit
 
-    time.sleep(3)
+    feed_url = profile_url.rsplit("/", 1)[0] + "/feed"
+
+    time.sleep(get_random_time())
 
     # Get to feed, this is where the pictures are
-    pics = browser.find_element(By.LINK_TEXT, 'Feed')
-    pics.click()
+    # Open the feed_url
+    browser.get(feed_url)
     time.sleep(3)
 
     # Populate the selector for date range to load all images
-    start_date = browser.find_element(By.NAME, 'activity-start-date')
+    start_date = browser.find_element(By.NAME, "start_date")
     start_date.send_keys(startdate)
-    end_date = browser.find_element(By.NAME, 'activity-end-date')
+    end_date = browser.find_element(By.NAME, "end_date")
     end_date.send_keys(enddate)
-    select = browser.find_element(By.ID, 'select-input-2')
-    select.send_keys('Photo')
+    select = browser.find_element(By.ID, "select-input-2")
+    select.send_keys("Photo")
     select.send_keys(Keys.ENTER)
 
     # This is the XPATH for the Apply button.
-    browser.find_element(By.XPATH,
-        '/html/body/div[2]/div/main/div/div/div[2]/div/form/button'
-        ).click()
+    browser.find_element(By.XPATH, '//*[@id="main"]/div/div/div[2]/div/form/button').click()
 
     try:
-        last_height = browser.execute_script(
-            "return document.body.scrollHeight"
-            )
+        last_height = browser.execute_script("return document.body.scrollHeight")
         counter = 0
         state = True
         while state is True:
             try:
                 counter += 1
-                button = WebDriverWait(browser, 7).until(
-                    EC.presence_of_element_located((
-                        By.XPATH, '//button[text()="Load more"]')))
+                button = WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Load more"]')))
                 button.click()
             except:
                 if counter == 1:
-                    logging.info('[!] No Loading button found!')
+                    logger.info("[-] No Loading button found, there may not be many pictures")
                 else:
-                    logging.debug('[!] No loading button found')
-            browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
+                    logger.error("[!] No loading button found")
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
             # Wait to load the page.
             time.sleep(2)
 
             # Calculate new scroll height and compare with last scroll height.
-            new_height = browser.execute_script(
-                "return document.body.scrollHeight")
+            new_height = browser.execute_script("return document.body.scrollHeight")
 
             if new_height == last_height:
-                logging.info('[!] Page fully loaded, finding images...')
+                logger.info("[!] Page fully loaded, finding images...")
                 state = False
 
             last_height = new_height
 
     except ElementNotVisibleException:
-        print('none')
+        logger.info("none")
 
     matches = re.findall(
         r'(?<=href=\")https:\/\/cdn\.mybrightwheel\.com\/media_images\/images\/[0-9a-zA-Z\/]*\.(?:jpg|png)(?="?)',
-        browser.page_source)
+        browser.page_source,
+    )
     count_matches = len(matches)
     if count_matches == 0:
-        logging.error(
-            '[!] No Images found to download! Check the source target page')
+        logger.error("[!] No Images found to download! Check the source target page")
     else:
-        logging.info('[!] Found {} files to download...'.format(count_matches))
+        logger.info("[!] Found {} files to download...".format(count_matches))
 
     return browser, matches
 
 
 def get_images(browser, matches):
-    """ Since Selenium doesn't handle saving images well, requests
+    """Since Selenium doesn't handle saving images well, requests
     can do this for us, but we need to pass it the cookies"""
     cookies = browser.get_cookies()
 
     session = requests.Session()
     for cookie in cookies:
-        session.cookies.set(cookie['name'], cookie['value'])
+        session.cookies.set(cookie["name"], cookie["value"])
 
     for match in matches:
         try:
             filename = match.split("/")[-1]
             request = session.get(match)
-            open('./pics/' + filename, 'wb').write(request.content)
-            logging.info('[-] - Downloading {}'.format(filename))
+            open("./pics/" + filename, "wb").write(request.content)
+            logger.info("[-] - Downloading {}".format(filename))
         except:
-            logging.error('[!] - Failed to save {}'.format(match))
+            logger.error("[!] - Failed to save {}".format(match))
 
     try:
         session.cookies.clear()
         browser.delete_all_cookies()
-        logging.info('[-] - Cleared cookies')
+        logger.info("[-] - Cleared cookies")
     except:
-        logging.error('[!] - Failed to clear cookies')
+        logger.error("[!] - Failed to clear cookies")
+
+
+def use_chrome_selenium():
+    """Init logging and do it"""
+
+    browser = uc.Chrome()
+    return browser
+
+
+def use_existing_chrome_session():
+    # Check if chrome is listening on port 9222
+    # If not, start it
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.debugger_address = "127.0.0.1:9222"
+    browser = webdriver.Chrome(options=chrome_options)
+    return browser
 
 
 def main():
-    """Init logging and do it"""
-    logging.basicConfig(filename='scraper.log', filemode='w')
+    parser = argparse.ArgumentParser(description="Brightwheel Scraper")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-c", "--chrome-selenium", action="store_true", help="Use existing Chrome session to login to Brightwheel")
+    group.add_argument("-e", "--chrome-session", action="store_true", help="Use existing Chrome session to login to Brightwheel")
+    parser.add_argument(
+        "-n", "--student-number", type=int, help="Select a student by number, indexed starting at 0. Look at the student list and count in order"
+    )
+    args = parser.parse_args()
 
-    browser = webdriver.Firefox()
+    if args.chrome_selenium:
+        browser = use_chrome_selenium()
+    elif args.chrome - session:
+        browser = use_existing_chrome_session()
+    else:
+        logger.error("[!] - No browser selected, exiting")
 
-    username, password, signin_url, kidlist_url, startdate, enddate = config_parser()
+    config = config_parser()
+    try:
+        username = config["bwuser"]
+        password = config["bwpass"]
+        signin_url = config["bwsignin"]
+        kidlist_url = config["bwlist"]
+        startdate = config["startdate"]
+        enddate = config["enddate"]
+    except KeyError:
+        logger.error("[!] - Check config file, missing required values")
+        raise SystemExit
 
-    browser = signme_in(browser, username, password, signin_url)
+    session = signme_in(browser, username, password, signin_url)
 
-    browser, matches = pic_finder(browser, kidlist_url, startdate, enddate)
+    session, matches = pic_finder(session, kidlist_url, startdate, enddate, args)
 
-    get_images(browser, matches)
+    get_images(session, matches)
 
 
 if __name__ == "__main__":
